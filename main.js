0920aa75b489926100997d99a384bf031dbed021
alvinnyen <alvinyen219@gmail.com>
2018-09-12 15:01:40 +0800

    test canvas 2d context

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const canvas = document.getElementById('myCanvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const canvasContext = canvas.getContext('2d');
+canvasContext.fillRect(100, 100, 100, 100);

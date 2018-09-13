34e874e9aae7a133085ef3ec2d5997869d35dc06
alvinnyen <alvinyen219@gmail.com>
2018-09-13 16:02:15 +0800

    test and add the note for the topic Drawing Rect.

--- main.js
+++ main.js
@@ -25,3 +25,15 @@ canvasContext.closePath(); // 繪製回到圓點的線
 
 canvasContext.stroke(); // 真正畫出來!!
 
+// 2. drawing rect
+// 不同於SVG，canvas只支援矩型的繪製
+// fillRect(x, y, width, height): 填滿的矩型
+// strokeRect(x, y, width, height): 邊框的矩型
+// clearRect(x, y, width, height): 清除指定矩形區域內的內容
+
+// 顏色的設定順序，沒特別設定就會跟著最近的那一個
+canvasContext.fillStyle = 'rgba(255, 0, 0, 0.1)';
+canvasContext.fillRect(25,25,100,100); // 粉色
+canvasContext.fillStyle = 'blue';
+canvasContext.fillRect(250,250,100,100); // 藍色
+canvasContext.fillRect(125,125,100,100); // 藍色
